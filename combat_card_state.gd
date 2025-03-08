dec0e5e9381277654e11f031be428edd41c89b88
John Nosek <john.nosek4@gmail.com>
2025-03-07 19:11:17 -0800

    export presets

--- combat_card_state.gd
+++ combat_card_state.gd
@@ -9,7 +9,8 @@ this is basically a data container for combat scene state
 
 var source_draw_pile: Array
 var draw_pile: Array
-var carry_pile: Array = []
+var bank_pile: Array = []
+var drawn_card: CombatCard
 var run: Array = []
 var run_probability: float
 var run_dmg: int
